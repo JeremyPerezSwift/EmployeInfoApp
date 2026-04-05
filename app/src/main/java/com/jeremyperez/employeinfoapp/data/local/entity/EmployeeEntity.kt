40452a1da7873ec2ec489102b1268c347e3cab15
JeremyPerezSwift <jeremyperez34000@gmail.com>
2026-04-05 09:08:16 +0200

    git commit -m "feat(data): add EmployeeEntity"

---
+++ app/src/main/java/com/jeremyperez/employeinfoapp/data/local/entity/EmployeeEntity.kt
@@ -0,0 +1,13 @@
+package com.jeremyperez.employeinfoapp.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "employees")
+data class EmployeeEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val name: String,
+    val designation: String,
+    val department: String,
+)
